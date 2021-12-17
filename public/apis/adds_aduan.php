8f71f33aa00e0517b0a77c4c9458cb4997f8174c
qsindojakarta1 <ichsanarrizqi@smkwikrama.sch.id>
2021-12-17 15:04:53 +0700

    Upload Images, 17122021 1504 Cipto Siswoyo

---
+++ public/apis/adds_aduan.php
@@ -0,0 +1,46 @@
+<?php
+require_once 'includes/Constants.php';
+class emp{}
+$image = $_POST['image'];
+$name = $_POST['name'];
+	
+	$random = random_word(20);
+	    $file_ext = ".png";
+		$file_name = $random.$file_ext;
+		$path = "images/";
+		
+		// sesuiakan ip address laptop/pc atau URL server
+		$actualpath = $path.$file_name;	
+
+
+$response = array();
+$query = mysqli_query($con, "INSERT INTO aduans_file (file_path,file_name,file_ext,name) VALUES ('$path','$file_name','$file_ext','$name')");
+
+
+
+if ($query){
+			file_put_contents($actualpath,base64_decode($image));
+			
+			$response = new emp();
+			$response->success = 1;
+			$response->message = "Successfully Uploaded";
+			die(json_encode($response));
+		} else{ 
+			$response = new emp();
+			$response->success = 0;
+			$response->message = "Error Upload image";
+			die(json_encode($response)); 
+		}
+
+
+function random_word($id = 20){
+		$pool = '1234567890abcdefghijkmnpqrstuvwxyz';
+		
+		$word = '';
+		for ($i = 0; $i < $id; $i++){
+			$word .= substr($pool, mt_rand(0, strlen($pool) -1), 1);
+		}
+		return $word; 
+	}
+echo json_encode($response);
+?>
