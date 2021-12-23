4b3668507e9ab5e1ee7bf23873ed9d9605f7dc4c
qsindojakarta1 <ichsanarrizqi@smkwikrama.sch.id>
2021-12-23 21:13:21 +0700

    View File, 23122021 2113 Cipto Siswoyo

--- public/apis/select_file_aduan.php
+++ public/apis/select_file_aduan.php
@@ -1,34 +1,24 @@
 <?php
 require_once 'includes/Constants.php';
-//$v_aduan_id = $_POST['txt_aduan_id'];
-$v_aduan_id = 75;
-$query = mysqli_query($con, "select * from aduans_file where aduan_id=$v_aduan_id");
-$data = array();
-$qry_array = array();
-$i = 0;
-$total = mysqli_num_rows($query);
-while ($row = mysqli_fetch_array($query)) {
-  $data['id'] = $row['id'];
-  $data['aduan_id'] = $row['aduan_id'];
-  $data['name'] = $row['name'];
-  $data['file_name'] = $row['file_name']; 
-  $data['file_path'] = $row['file_path'];
-  $data['file_ext'] = $row['file_ext'];
-  $data['created_at'] = $row['created_at'];
-  $data['updated_at'] = $row['updated_at'];
-  $qry_array[$i] = $data;
-  $i++;
+
+if(isset($_GET['id_aduan'])){
+  $kd=$_GET['id_aduan'];
+ }
+
+$query = mysqli_query($con,"SELECT * FROM aduans_file where aduan_id = $kd ORDER BY id desc");
+$json  = '{"aduans_file": [';
+
+while ($row = mysqli_fetch_array ($query)) {
+
+$char = '"';
+$json .= '{"id":"'.$row['id'].'",
+"judul":"'.str_replace($char,'`',strip_tags($row['name'])).'",
+"isi":"'.str_replace($char,'`',strip_tags($row['contents'])).'",
+"gambar":"images/'.$row['file_name'].'"},';
 }
 
-if($query){
-  $response['success'] = 'true';
-  $response['message'] = 'Data Loaded Successfully';
-  $response['total'] = $total;
-  $response['data'] = $qry_array;
-}else{
-  $response['success'] = 'false';
-  $response['message'] = 'Data Loading Failed';
-}
+$json = substr($json,0,strlen($json)-1);
+$json .= ']}';
+echo $json;
 
-echo json_encode($response);
 ?>
