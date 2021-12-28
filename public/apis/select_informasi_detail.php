fcee4eb3b9e2a0a8dcde5afd106a9ca0acba96d8
qsindojakarta1 <ichsanarrizqi@smkwikrama.sch.id>
2021-12-28 17:59:52 +0700

    Image detail info, 28122021 1756 Cipto Siswoyo

--- public/apis/select_informasi_detail.php
+++ public/apis/select_informasi_detail.php
@@ -1,8 +1,14 @@
 <?php
+
+$kd="";
 require_once 'includes/Constants.php';
 
-$query = mysqli_query($con,"
-select
+//$kd = $_GET['idberita'];
+if(isset($_GET['id_informasi'])){
+ $kd=$_GET['id_informasi'];
+}
+
+$query = mysqli_query($con,"select
 informasis.*,
 desas.id as desas_id ,desas.kecamatan_id,desas.nama_desa,desas.background,
 kecamatans.id as kecamatans_id,kecamatans.kabupaten_id,kecamatans.nama_kecamatan,
@@ -22,26 +28,28 @@ on
 desas.kecamatan_id = kecamatans.id		
 left join kabupatens
 on
-kecamatans.kabupaten_id = kabupatens.id
-");
+kecamatans.kabupaten_id = kabupatens.id where informasis.id= $kd ");
 $json  = '{"informasi": [';
 
-while ($row = mysqli_fetch_array ($query)) {
+while($row=mysqli_fetch_array($query))
+{
 
 $char = '"';
+
 $json .= '{"id":"'.$row['id'].'",
-"judul":"'.str_replace($char,'`',strip_tags($row['judul'])).'",
-"nama_desa":"'.str_replace($char,'`',strip_tags($row['nama_desa'])).'",
-"isi":"'.str_replace($char,'`',strip_tags($row['deskripsi'])).'",
-"gambar":"images/'.$row['images'].'",
-"created_at":"'.str_replace($char,'`',strip_tags($row['created_at'])).'",
-"nama_kategori":"'.str_replace($char,'`',strip_tags($row['nama_kategori'])).'"
-},';
+  "judul":"'.str_replace($char,'`',strip_tags($row['judul'])).'",
+  "nama_desa":"'.str_replace($char,'`',strip_tags($row['nama_desa'])).'",
+  "isi":"'.str_replace($char,'`',strip_tags($row['deskripsi'])).'",
+  "gambar":"images/'.$row['images'].'",
+  "created_at":"'.str_replace($char,'`',strip_tags($row['created_at'])).'",
+  "nama_kategori":"'.str_replace($char,'`',strip_tags($row['nama_kategori'])).'"
+  },';
 
 }
-
+// buat menghilangkan koma diakhir array
 $json = substr($json,0,strlen($json)-1);
-$json .= ']}';
-echo $json;
 
+$json .= ']}';
+// print json
+echo $json;
 ?>
